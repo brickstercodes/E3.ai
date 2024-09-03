b20526cb0debd2290e10fe212b4a377e66fcc4ab
Anugrah Shetty <146320037+brickstercodes@users.noreply.github.com>
2024-09-03 23:27:36 +0530

    Update backend.py (allowed origins)

--- backend.py
+++ backend.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://e3-ml8nhuq94-anugrahs-projects-49e1c22d.vercel.app"],
+    allow_origins=["https://e3-ai.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
