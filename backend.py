8aafa800c7ddae50dbd3ec42cf56e9f926623289
Anugrah Shetty <146320037+brickstercodes@users.noreply.github.com>
2024-09-07 12:14:09 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -182,5 +182,5 @@ async def read_root():
 if __name__ == "__main__":
     import uvicorn
     import os
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 10000))
     uvicorn.run(app, host="0.0.0.0", port=port)
